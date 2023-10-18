606a4347a402ce0a2ed210935d2d8e16d0124816
biohackerellie <epkerns@gmail.com>
2023-10-18 08:34:34 -0600

    commit

--- Week1/guessingGame.cpp
+++ Week1/guessingGame.cpp
@@ -1,26 +1,33 @@
+
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
 using namespace std;
 
 int main() {
-	srand(time(0));
-	int randomNumber = rand() % 100 + 1;
-	int guess;
-
-	cout << "Guess the number between 1 and 100: ";
-
+	char playAgain;
 	do {
-		cin >> guess;
+		srand(time(0));
+		int randomNumber = rand() % 100 + 1;
+		int guess;
+		cout << "Guess the number between 1 and 100: ";
+		do {
+				cin >> guess;
 
-		if(guess < randomNumber) {
-			cout << "Too low";
-		} else if(guess > randomNumber) {
-			cout << "Too high!! moron";
-		}
-	} while(guess != randomNumber);
+				if(guess < randomNumber) {
+					cout << "Too low ";
+				} else if(guess > randomNumber) {
+					cout << "Too high!! ";
+				}
+		} while(guess != randomNumber);
 
-	cout << "alright alright you win" << randomNumber<< " was the number" << endl;
+		cout << "alright alright you win" << randomNumber<< " was the number" << endl;
+
+		cout << "Do you want to play again? (y/n): ";
+		cin >> playAgain;
+		} while (playAgain == 'y' || playAgain == 'Y');
 
 	return 0;
-}
+} 
+
+
